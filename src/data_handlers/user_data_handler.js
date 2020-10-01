5141b41655997c20bf3346f314d270c4dd3cc4aa
matsvei.murashka <stranger2626@gmail.com>
2020-10-01 17:48:34 +0300

    added isMatchingAllSearchParams

--- src/data_handlers/user_data_handler.js
+++ src/data_handlers/user_data_handler.js
@@ -1,19 +1,19 @@
 const axios = require('axios').default
 
-class UserDataHanlder {
+module.exports = class UserDataHandler {
   constructor () {
     this.users = []
   }
 
   async loadUsers () {
-    const response = await axios.get('https://locahost:3000').catch(err => {
+    const response = await axios.get('http://localhost:3000/users').catch(err => {
       throw new Error(`Failed to load users data: ${err}`)
     })
     this.users = response.data
   }
 
   getUserEmailsList () {
-    if (!this.users) throw new Error('No users loaded')
+    if (this.users.length === 0) throw new Error('No users loaded!')
     const arrayOfEmails = this.users.map(user => user.email)
     const listOfUSerEmails = arrayOfEmails.join(';')
     return listOfUSerEmails
@@ -23,8 +23,24 @@ class UserDataHanlder {
     return this.users.length
   }
 
+  isMatchingAllSearchParams (user, searchParamsObject) {
+    let isMatching = true
+    for (const searchParam in searchParamsObject) {
+      if (user[searchParam] !== searchParamsObject[searchParam]) {
+        isMatching = false
+      }
+      if (!isMatching) break
+    }
+    return isMatching
+  }
+
   findUsers (searchParamsObject) {
+    if (!searchParamsObject) throw new Error('No search parameters provoded!')
+    if (this.users.length === 0) throw new Error('No users loaded!')
+    const matchingUsers = this.users.filter(this.isMatchingAllSearchParams)
+    if (matchingUsers.length === 0) throw new Error('No matching users found!')
+    return matchingUsers
   }
 }
 
-module.exports = UserDataHanlder
+
